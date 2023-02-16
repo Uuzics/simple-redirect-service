9bd855828295ed836fff6bbd9abd6bbef67812b8
Uuzics <87236102+Uuzics@users.noreply.github.com>
2023-02-16 15:13:05 +0800

    Init Maven project

---
+++ src/main/java/org/uuzics/simpleredirectservice/SimpleRedirectServiceApplication.java
@@ -0,0 +1,13 @@
+package org.uuzics.simpleredirectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRedirectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRedirectServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/uuzics/simpleredirectservice/SimpleRedirectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uuzics.simpleredirectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRedirectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
