4f31240d18154576458a040dd226e75702ee581c
Uuzics <87236102+Uuzics@users.noreply.github.com>
2023-02-21 16:28:22 +0800

    [doc]add license

--- src/main/java/org/uuzics/simpleredirectservice/controller/RedirectController.java
+++ src/main/java/org/uuzics/simpleredirectservice/controller/RedirectController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Uuzics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.uuzics.simpleredirectservice.controller;
 
 import org.springframework.stereotype.Controller;
@@ -11,11 +26,21 @@ import org.uuzics.simpleredirectservice.service.RedirectionService;
 import javax.annotation.Resource;
 import java.util.List;
 
+/**
+ * Controller for URL redirection
+ */
 @Controller
 public class RedirectController {
     @Resource
     private RedirectionService redirectionService;
 
+    /**
+     * Redirect to destination URL given a resource ID
+     *
+     * @param resourceId resource ID
+     * @param model      Spring Boot model
+     * @return name of Spring Boot view
+     */
     @RequestMapping(value = "/{resourceId}", method = RequestMethod.GET, produces = "text/html;charset=UTF-8")
     public String redirect(@PathVariable("resourceId") String resourceId, Model model) {
         // Retrieve redirect url and redirect type
@@ -27,6 +52,7 @@ public class RedirectController {
         String redirectType = tempRedirectionList.get(0).getRedirectType();
 
         // Validation
+        // TODO serious validations on URL
         if (null == redirectUrl || null == redirectType) {
             return "error";
         }
@@ -44,6 +70,7 @@ public class RedirectController {
                 model.addAttribute("redirectUrl", redirectUrl);
                 return "redirect/JavaScriptRedirect.html";
             default:
+                // TODO maybe another error page?
                 return "http301Redirect:" + redirectUrl;
         }
     }
--- src/main/java/org/uuzics/simpleredirectservice/view/Http301RedirectViewResolver.java
+++ src/main/java/org/uuzics/simpleredirectservice/view/Http301RedirectViewResolver.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Uuzics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.uuzics.simpleredirectservice.view;
 
 import org.springframework.http.HttpStatus;
@@ -8,10 +23,21 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Locale;
 
+/**
+ * Customized view resolver for HTTP301 redirection
+ */
 @Component
 public class Http301RedirectViewResolver implements ViewResolver {
     public static final String HTTP_301_REDIRECT_PREFIX = "http301Redirect:";
 
+    /**
+     * Redirect by HTTP301 response
+     *
+     * @param viewName view name
+     * @param locale locale
+     * @return redirection view
+     * @throws Exception exception
+     */
     @Override
     public View resolveViewName(String viewName, Locale locale) throws Exception {
         if (!viewName.startsWith(HTTP_301_REDIRECT_PREFIX)) {
